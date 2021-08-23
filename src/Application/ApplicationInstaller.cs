b288d3dffccf456dd7dd058114dd69466c86706f
AmadorSV <AmadorSV@users.noreply.github.com>
2021-08-22 20:11:43 -0400

    Add fluent validation and MediatR

---
+++ src/Application/ApplicationInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ApplicationInstaller
+    {
+        public static IServiceCollection InstallApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+            return services;
+        }
+    }
+}
