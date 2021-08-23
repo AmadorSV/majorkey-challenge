06b6fbdf2683e872449b60bad2b4e8620ea17fd9
AmadorSV <AmadorSV@users.noreply.github.com>
2021-08-22 21:03:13 -0400

    Add validation request filter

---
+++ src/RESTApi/Filters/ValidationFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTApi.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors.Select(y => y.ErrorMessage))
+                    .ToList();
+
+                context.Result = new BadRequestObjectResult(errors);
+                return;
+            }
+
+            await next();
+        }
+    }
+}
