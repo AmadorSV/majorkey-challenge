31c728cc8625a8209b62674189249db4ea4ff937
AmadorSV <AmadorSV@users.noreply.github.com>
2021-08-26 03:33:40 -0400

    Add Integration testing and unit testing

---
+++ src/Tests/Application/ServiceRequest/Command/UpdateServiceRequestCmdTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Common.Exception;
+using Application.Features.ServiceRequest.Commands;
+using Domain.Enum;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Tests.Application.ServiceRequest
+{
+    using static Testing;
+    using static Shared;
+    
+    public class UpdateServiceRequestCmdTest
+    {
+        [Test]
+        public async Task UpdateRequestt_WithFilledCmd_ShouldUpdate()
+        {
+            // Arrage
+
+            var updateCommand = new UpdateServiceRequestCmd
+            {
+                Description = "New Any Description",
+                BuildingCode = "new123456764532",
+                CurrentStatus = CurrentStatus.Canceled,
+                ModifiedBy = "NewUser"
+            };
+            
+
+            // Act
+
+            var createdGuid = await CreateServiceRequest();
+            var currentDate = DateTime.Now;
+            
+            updateCommand.Id = createdGuid;
+            await SendAsync(updateCommand);
+            var updatedEntity = await FindAsync<Domain.Model.ServiceRequest>(createdGuid);
+            
+            // Assert
+
+            updatedEntity.Should().NotBeNull();
+            updatedEntity.Description.Should().Be(updateCommand.Description);
+            updatedEntity.BuildingCode.Should().Be(updateCommand.BuildingCode);
+            updatedEntity.ModifiedBy.Should().Be(updateCommand.ModifiedBy);
+            updatedEntity.ModifiedDate.Should().BeCloseTo(currentDate,TimeSpan.FromSeconds(2));
+
+        }
+        
+        [Test]
+        public void UpdateRequest_WithNotExistingId_ReturnsNotFound()
+        {
+            // Arrage
+            var updateCommand = new UpdateServiceRequestCmd
+            {
+                Id = new Guid()
+            };
+            
+            // Assert
+
+            Assert.ThrowsAsync<NotFoundException>(async () => await SendAsync(updateCommand));
+        }
+       
+    }
+};
---
+++ src/Tests/Application/ServiceRequest/Query/GetServiceRequestTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Common.Exception;
+using Application.Features.ServiceRequest.Commands;
+using Application.Features.ServiceRequest.Queries;
+using Domain.Enum;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests.Application.ServiceRequest.Query
+{
+    using static Testing;
+    using static Shared;
+    
+    public class GetServiceRequestTest
+    {
+        [Test]
+        public async Task GetServiceRequest_NoExistingId_NotFound()
+        {
+            // Arrage
+            var qry = new GetServiceRequestQry(new Guid());
+
+            // Act
+            // Assert
+            Assert.ThrowsAsync<NotFoundException>(async () => await SendAsync(qry));
+        }
+        
+        [Test]
+        public async Task GetServiceRequest_WithExistingId_NotNull()
+        {
+            // Arrage
+            var id = await CreateServiceRequest();
+            var qry = new GetServiceRequestQry(id);
+
+            // Act
+            var result = await SendAsync(qry);
+            // Assert
+            
+            result.Should().NotBeNull();
+        }
+    }
+}
